e0d799fa7ba66006d613d42db87e720c1237f45c
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-27 06:52:01 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springboots3upload/SpringbootS3UploadApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboots3upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootS3UploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootS3UploadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springboots3upload/SpringbootS3UploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboots3upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootS3UploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
