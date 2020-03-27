92be420596f8ab6ac6436e7afb8cd665c564f619
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-27 07:43:17 +0700

    #7 - Add save method in FileStore Service

--- src/main/java/com/hendisantika/springboots3upload/filesstore/FileStore.java
+++ src/main/java/com/hendisantika/springboots3upload/filesstore/FileStore.java
@@ -1,8 +1,14 @@
 package com.hendisantika.springboots3upload.filesstore;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,5 +26,22 @@ public class FileStore {
     @Autowired
     public FileStore(AmazonS3 s3) {
         this.s3 = s3;
+    }
+
+    public void save(String path, String fileName, Optional<Map<String, String>> optionalMetadata,
+                     InputStream inputStream) {
+        ObjectMetadata metadata = new ObjectMetadata();
+
+        optionalMetadata.ifPresent(map -> {
+            if (!map.isEmpty()) {
+                map.forEach(metadata::addUserMetadata);
+            }
+        });
+
+        try {
+            s3.putObject(path, fileName, inputStream, metadata);
+        } catch (AmazonS3Exception e) {
+            throw new IllegalStateException("Failed to store file to S3.", e);
+        }
     }
 }
