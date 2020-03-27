013e69c15d4d05ceabd6b0e16ec48c79e002c9d8
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-27 07:00:08 +0700

    #3 - Add AWSConfig

---
+++ src/main/java/com/hendisantika/springboots3upload/config/AWSConfig.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboots3upload.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-s3-upload
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/03/20
+ * Time: 06.59
+ */
+public class AWSConfig {
+}
