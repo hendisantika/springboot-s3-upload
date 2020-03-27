0e48d752ca59fd91b112a4f95c1ac21642c5be3f
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-27 07:58:22 +0700

    #10 - Add UserProfileController

---
+++ src/main/java/com/hendisantika/springboots3upload/controller/UserProfileController.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springboots3upload.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-s3-upload
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/03/20
+ * Time: 07.57
+ */
+@RestController
+@RequestMapping("api/v1/user-profile")
+public class UserProfileController {
+}
