70e82ce88a1f776d8ee38befac80af0dbc992183
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-27 08:46:05 +0700

    #16 - Prepare uploadUserProfileImage method & API

--- src/main/java/com/hendisantika/springboots3upload/controller/UserProfileController.java
+++ src/main/java/com/hendisantika/springboots3upload/controller/UserProfileController.java
@@ -4,10 +4,14 @@ import com.hendisantika.springboots3upload.profile.UserProfile;
 import com.hendisantika.springboots3upload.service.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +35,10 @@ public class UserProfileController {
     @GetMapping
     public List<UserProfile> getUserProfiles() {
         return userProfileService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
+                                       @RequestParam("file") MultipartFile file) {
+        userProfileService.uploadUserProfileImage(userProfileId, file);
     }
 }
--- src/main/java/com/hendisantika/springboots3upload/service/UserProfileService.java
+++ src/main/java/com/hendisantika/springboots3upload/service/UserProfileService.java
@@ -3,8 +3,10 @@ package com.hendisantika.springboots3upload.service;
 import com.hendisantika.springboots3upload.profile.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +28,15 @@ public class UserProfileService {
 
     public List<UserProfile> getUserProfiles() {
         return userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        /*
+         * 1. Check if image is not empty
+         * 2. If file is an image
+         * 3. The user is exists in our database
+         * 4. Grab some metadata from file if any
+         * 5. Store the image in S3 and update database (userProfileImageLink) with S3 image link
+         * */
     }
 }
